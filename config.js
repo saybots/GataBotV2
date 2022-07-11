efbc1f636322549613f89d0312c820e00cc2f6f5
saybots <109103074+saybots@users.noreply.github.com>
2022-07-11 13:22:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['593993684821' , '51993042301' , '529996125657'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['5219992095479' , '972557048356'] // No tiene utilidad
-global.prems = ['5219996125657' , '5219991402134'] // No tiene utilidad
+global.owner = ['593968992487' , '' , '5'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['52199929' , '948356'] // No tiene utilidad
+global.prems = ['521999617' , '521402134'] // No tiene utilidad
 
-// Nota: Puedes contactarme si necesitas ayuda con algo al +994407312387 (Solo temas de la instalación)
-// => Solo dudas sobre la instalación, temas generales contactarme al +593968585383 (Temas serios, si vas a intervenir al chat para molesrtar seras bloqueado/a) 
+// Nota: Puedes contactarme si necesitas ayuda con algo al + (Solo temas de la instalación)
+// => Solo dudas sobre la instalación, temas generales contactarme al +5983 (Temas serios, si vas a intervenir al chat para molesrtar seras bloqueado/a) 
 // => NO ayudo a hacer Bots, ni crearlos, ni editarlos 
-// - Este es el Canal de The Shadow Brokers por si te interesa su canal de YouTube en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
+// - Este BOT ES DE SU CARDING ACADEMY por si te interesa NO TE DARE LOS REPOSITORIOS XD
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -49,8 +49,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'GataBot 🐈'
-global.author = 'Gata Dios'
+global.packname = 'SAYBot 🐈'
+global.author = 'SAY BOTS'
 
 
 //global.wait = '*Esperé un momento..*'
