a177b95027314b0bc94f039e82ad767f6d6a337a
saybots <109103074+saybots@users.noreply.github.com>
2022-07-11 13:23:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
-global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
+global.linkGC = ['https://chat.whatsapp.com/JlomzjGUYPfyJ' , 'https://chat.whatsapydWBE1ZN3EoY0hRs' , 'https://chat.whatxx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/e3bqgwlb'] // No tiene utilidad 
+global.channelYT = ['https://www.youtube.com/'] // No tiene utilidad
 global.owner = ['593968992487' , '' , '5'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
 global.mods = ['52199929' , '948356'] // No tiene utilidad
 global.prems = ['521999617' , '521402134'] // No tiene utilidad
